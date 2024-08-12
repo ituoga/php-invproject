064bef9247606d8fbe705b8b48ce8363ef4b2e4d
Mindaugas Žvirblis <mz@9g.lt>
2024-08-12 20:19:17 +0300

    build

---
+++ app/Models/Domain.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use Exception;
+use Illuminate\Support\Str;
+use Stancl\Tenancy\Database\Models\Domain as BaseDomain;
+
+/**
+ * @property int $id
+ * @property int $tenant_id
+ * @property string $domain
+ * @property bool $is_primary
+ * @property bool $is_fallback
+ * @property string $certificate_status
+ * @property Tenant $tenant
+ */
+class Domain extends BaseDomain
+{
+    protected $casts = [
+        'is_primary' => 'bool',
+        'is_fallback' => 'bool',
+    ];
+
+    public static function booted()
+    {
+        static::saving(function (self $model) {
+            if (in_array($model->domain, config('saas.reserved_subdomains'))) {
+                throw new Exception($model->domain);
+            }
+        });
+
+        static::updating(function (self $model) {
+            if ($model->getAttribute('domain') !== $model->getOriginal('domain')) {
+                throw new Exception('Domain cannot be changed');
+            }
+        });
+
+        static::saved(function (self $model) {
+            // There can only be one of these
+            $uniqueKeys = ['is_primary', 'is_fallback'];
+
+            foreach ($uniqueKeys as $key) {
+                if ($model->$key) {
+                    $model->tenant->domains()
+                        ->where('id', '!=', $model->id)
+                        ->update([$key => false]);
+                }
+            }
+        });
+    }
+
+    public static function domainFromSubdomain(string $subdomain): string
+    {
+        return $subdomain.'.'.config('tenancy.central_domains')[0];
+    }
+
+    public function makePrimary(): self
+    {
+        $this->update([
+            'is_primary' => true,
+        ]);
+
+        $this->tenant->setRelation('primary_domain', $this);
+
+        return $this;
+    }
+
+    public function makeFallback(): self
+    {
+        $this->update([
+            'is_fallback' => true,
+        ]);
+
+        $this->tenant->setRelation('fallback_domain', $this);
+
+        return $this;
+    }
+
+    public function isSubdomain(): bool
+    {
+        return ! Str::contains($this->domain, '.');
+    }
+
+    /**
+     * Get the domain type.
+     * Returns 'subdomain' or 'domain'.
+     */
+    public function getTypeAttribute(): string
+    {
+        return $this->isSubdomain() ? 'subdomain' : 'domain';
+    }
+}
