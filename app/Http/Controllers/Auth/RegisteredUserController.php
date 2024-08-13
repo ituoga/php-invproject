6d017f55e31a4753cdf82ad9fd8e2a4ed2111c97
Mindaugas Žvirblis <mz@9g.lt>
2024-08-13 18:19:22 +0300

    code snifer

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,6 +36,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'g-recaptcha-response' => 'required|recaptchav3:register,0.5'
         ]);
 
         $user = User::create([
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 <x-guest-layout>
+    <x-input-error :messages="$errors->get('g-recaptcha-response')" class="mb-4" />
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,6 +51,7 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        {!! RecaptchaV3::field('register') !!}
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
@@ -59,4 +62,6 @@
             </x-primary-button>
         </div>
     </form>
+
+    {!! RecaptchaV3::initJs() !!}
 </x-guest-layout>
