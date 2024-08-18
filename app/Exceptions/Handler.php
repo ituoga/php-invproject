29fb803119068b85f894219a484069e790e9a360
Martynas Sklizmantas <saint@ghost.lt>
2024-08-18 17:46:12 +0200

    laravel 11

--- app/Exceptions/Handler.php
+++
@@ -1,8 +0,0 @@
-use Sentry\Laravel\Integration;
-
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            Integration::captureUnhandledException($e);
-        });
-    }
