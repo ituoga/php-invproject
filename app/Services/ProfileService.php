9a7c5634a045f37a2decd532581345eacd6d6177
Mindaugas Žvirblis <mz@9g.lt>
2024-08-21 18:29:53 +0300

    cleanup

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -34,16 +34,8 @@ class ProfileService extends BaseService implements BaseServiceInterface
     {
 
         $service = app(ConfigService::class);
-//        $request = app(ProfileUpdateRequest::class);
 
         $service->create($data);
-        // $request->user()->fill($request->validated());
-
-        // if ($request->user()->isDirty('email')) {
-        //     $request->user()->email_verified_at = null;
-        // }
-
-        // $request->user()->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
