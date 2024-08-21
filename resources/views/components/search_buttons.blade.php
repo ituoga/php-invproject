b808b1de6cc813bd1be66497ffe532bcf9ea536e
Mindaugas Žvirblis <mz@9g.lt>
2024-08-21 21:51:25 +0300

    typo

--- resources/views/components/search_buttons.blade.php
+++ resources/views/components/search_buttons.blade.php
@@ -11,7 +11,7 @@
             <label for="id-047724" class="visually-hidden">{{__("ieškoti")}}</label>
             <div class="form-regular__wrap">
                 <button class="form-regular__icon"><i class="icon-search" aria-hidden="true"></i></button>
-                <input type="text" name="q" placeholder="{{__("Ieškoti")}}" id="id-047724">
+                <input type="text" name="q" placeholder="{{__("ieškoti")}}">
             </div>
         </form>
     </div>
